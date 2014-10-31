489bff47259a9b38c3ce1b103c3c472f9456230c
Peter Green <pitakakariki@gmail.com>
2014-11-01 12:55:41 +1300

    Outline drop1test

--- R/testFactories.R
+++ R/testFactories.R
@@ -11,3 +11,15 @@ fixed <- function(xname, test) {
 
 
 }
+
+#
+# Build a test factory using drop1
+#
+drop1test <- function() {
+    
+    # formula for dropped variable    
+    
+    # specify comparison test
+    
+    # use drop1
+}

d5dfe39c0ec6a4d78a81391e433de0f1575a2a6c
unknown <greenp@GREENP-W7L.landcare.ad.landcareresearch.co.nz>
2014-11-02 20:02:41 +1300

    Test infrastructure

--- R/testFactories.R
+++ R/testFactories.R
@@ -16,10 +16,25 @@ fixed <- function(xname, test) {
 # Build a test factory using drop1
 #
 drop1test <- function() {
-    
-    # formula for dropped variable    
-    
+
+    # formula for dropped variable
+
     # specify comparison test
-    
+
     # use drop1
+}
+
+#
+# Create a working `sumFun` from a comparison function
+#
+drop1wrap <- function(f, method="abc") {
+
+    function(a, b, ...) {
+
+
+
+        f(a, b)
+
+        attr(rval, "method") <- method
+    }
 }

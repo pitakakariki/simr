82918952e4bc4a4277d3e7eaa47dc7f0cd2dc6d2
unknown <greenp@GREENP-W7L.landcare.ad.landcareresearch.co.nz>
2015-12-16 16:30:37 +1300

    cleanup for CHECK

--- R/glm.R
+++ R/glm.R
@@ -1,8 +1,8 @@
 #' @export
-doFit.glm <- function(y, model, subset, ...) {
+doFit.glm <- function(y, fit, subset, ...) {
 
-    newData <- getData(model)
-    responseName <- as.character(formula(model)[[2]])
+    newData <- getData(fit)
+    responseName <- as.character(formula(fit)[[2]])
 
     # hack for binomial
     if(responseName[1] == "cbind") {
@@ -15,26 +15,11 @@ doFit.glm <- function(y, model, subset, ...) {
 
     newData <- newData[subset, ]
 
-    model$call[["data"]] <- quote(newData)
+    fit$call[["data"]] <- quote(newData)
 
-    rval <- eval(model$call)
+    rval <- eval(fit$call)
 
     return(rval)
 }
 
-#' @export
-getData.glm <- function(x) x$data
 
-#doSim.glm <- doSim.default
-
-fixef.glm <- coef
-
-#getDefaultTest.glm <- function() {}
-
-counts <- c(18,17,15,20,10,20,25,13,12)
-outcome <- gl(3,1,9)
-treatment <- gl(3,3)
-#print(d.AD <- data.frame(treatment, outcome, counts))
-glm.D93 <- glm(counts ~ outcome + treatment, family = poisson())
-anova(glm.D93)
-summary(glm.D93)

6b0c56a4b902084a58b89a815efc5f91203dd47a
pitakakariki <pitakakariki@gmail.com>
2016-01-25 15:29:43 +1300

    powerCurve along and within tests

--- tests/testthat/test_powerCurve.R
+++ tests/testthat/test_powerCurve.R
@@ -2,10 +2,19 @@ context("powerCurve")
 
 test_that("", {
 
-    pc <- powerCurve(fm1, nsim=3)
+    pc1 <- powerCurve(fm1, nsim=3)
 
+    expect_equal(nrow(pc1$warnings), 0)
+    expect_equal(nrow(pc1$errors), 0)
 
-    expect_equal(nrow(pc$warnings), 0)
-    expect_equal(nrow(pc$errors), 0)
+    pc2 <- powerCurve(fm1, along="g", nsim=3)
+
+    expect_equal(nrow(pc2$warnings), 0)
+    expect_equal(nrow(pc2$errors), 0)
+
+    pc3 <- powerCurve(fm1, within="x+g", nsim=3)
+
+    expect_equal(nrow(pc3$warnings), 0)
+    expect_equal(nrow(pc3$errors), 0)
 
 })

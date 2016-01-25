092775bfc721c44b31da9ab8983c6774e1f0fa26
pitakakariki <pitakakariki@gmail.com>
2016-01-25 23:08:05 +1300

    unit test extend factor

--- tests/testthat/test_extend.r
+++ tests/testthat/test_extend.r
@@ -2,17 +2,22 @@ context('extend')
 
 test_that('extended model has correct dimensions', {
 
-    fm1x <- extend(fm1, along="x", n=20)
+    x1 <- extend(fm1, along="x", n=20)
 
-    expect_equal(nrow(getData(fm1x)), 2*nrow(getData(fm1)))
+    expect_equal(nrow(getData(x1)), 2*nrow(getData(fm1)))
+    
+    
+    x2 <- extend(fm1, along="g", n=15)
+    
+    expect_equal(nrow(getData(x2)), 5*nrow(getData(fm1)))
 
     
-    fm1w <- extend(fm1, within="x+g", n=3)
+    x3 <- extend(fm1, within="x+g", n=3)
     
-    expect_equal(nrow(getData(fm1w)), 3*nrow(getData(fm1)))
+    expect_equal(nrow(getData(x3)), 3*nrow(getData(fm1)))
 
     
-    flmx <- extend(flm, along="x", n=20)
+    x4 <- extend(flm, along="x", n=20)
 
-    expect_equal(nrow(getData(flmx)), 2*nrow(getData(flm)))
+    expect_equal(nrow(getData(x4)), 2*nrow(getData(flm)))
 })

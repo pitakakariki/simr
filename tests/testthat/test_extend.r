18d53c486571ab5179190f5d8b622345d5d0b0b2
pitakakariki <pitakakariki@gmail.com>
2016-01-25 20:01:36 +1300

    tests for extend

--- tests/testthat/test_extend.r
+++ tests/testthat/test_extend.r
@@ -1,61 +1,12 @@
-## test that noopt works to copy a merMod
-
-## test that new frame has the right size
-
-## test that fixefs are the same after extend
-
-
-
-
-##
-## Things that need to be the same after `extend`, and things that need to be different.
-##
-
-# (Based on the `simulate` function.)
-
-
-
-# X [getME(obj, 'X')] and n [nrow(X)] should change as appropriate.
-# [ncol(X)] should be the same.
-
-# fixef(obj), getME(obj, 'beta')
-
-# Z and lambda?
-
-if(FALSE) {
-
 context('extend')
 
-  
-  ## BAD MODEL GIVES WARNINGS???
-a1 <- lmer(Carbon ~ Position * Mgmt + Year + (Year | Cluster/Mgmt/Block/Position), kiwifruit)
-ax <- extend(a1, 'Year', values=seq(2004, 2014, 2))
-
-test_that('extended model preserves original parameters', {
-    
-    # Should still be a model object
-    expect_is(ax, 'merMod')
-    
-    expect_equal(sigma(ax), sigma(a1))
-    
-    expect_equal(getME(ax, 'beta'), getME(a1, 'beta'))
-    expect_equal(fixef(ax), fixef(a1))
-    
-    expect_equal(getME(ax, 'Lambda'), getME(a1, 'Lambda'))
-    
-    expect_equal(ranef(ax), ranef(a1)) 
-    
-})
-
 test_that('extended model has correct dimensions', {
-    
-    
-    
-    
-    
+
+    fm1x <- extend(fm1, along="x", n=20)
+
+    expect_equal(nrow(getData(fm1x)), 2*nrow(getData(fm1)))
+
+    flmx <- extend(flm, along="x", n=20)
+
+    expect_equal(nrow(getData(flmx)), 2*nrow(getData(flm)))
 })
-  
-  
-  
-  
-}

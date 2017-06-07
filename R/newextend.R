de006a3fb8dba6866f6a056253e11bfd8364ce44
pitakakariki <pitakakariki@gmail.com>
2017-06-08 10:57:38 +1200

    Old NSE code here

--- R/newextend.R
+++ R/newextend.R
@@ -6,6 +6,8 @@ newextend_ <- function(object, along, n, addn, values, addvalues, where) {
     eval(bquote(newextend(object, .(along), n, addn, values, addvalues, where)))
 }
 
+#### Get rid of NSE, maybe think about adding it later?
+
 newextend <- function(object, along, n, addn, values, addvalues, where) {
 
     if(missing(along)) stop("You need to specity a variable with 'along' or 'within'")

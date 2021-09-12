169b05173dd11e6f237228db41d12531679e1ed9
Pablo Bernabeu <pcbernabeu@gmail.com>
2021-09-12 14:55:03 +0100

    Correction of label

--- R/powerggplot.R
+++ R/powerggplot.R
@@ -10,7 +10,7 @@ powerggplot = function(x) {
     geom_errorbar(ymin = data.frame(summary(x))$lower, 
                   ymax = data.frame(summary(x))$upper, 
                   width = 50, color = 'black') +
-    scale_x_continuous(name = 'Number of participants', breaks = x$nlevels) +
+    scale_x_continuous(name = paste('Levels of', x$along), breaks = x$nlevels) +
     scale_y_continuous(name = 'Power', limits = c(0, 1), breaks = c(0, .2, .4, .6, .8, 1),
                        labels = c('0%', '20%', '40%', '60%', '80%', '100%')) +
     theme_classic() + theme(axis.title = element_text(size = 16), 

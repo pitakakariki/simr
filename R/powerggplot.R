f30331eb01518561e109e2df1a51ab90fdd44cc1
Pablo Bernabeu <pcbernabeu@gmail.com>
2021-09-12 15:42:21 +0100

    Update powerggplot.R
    Minor edits to code

--- R/powerggplot.R
+++ R/powerggplot.R
@@ -3,14 +3,12 @@ library(tidyverse)  # data wrangling and plotting
 
 powerggplot = function(x) {
   data.frame(summary(x)) %>%
-    ggplot(aes(y = mean, x = nlevels)) +
+    ggplot(aes(y = mean, x = nlevels, ymin = lower, ymax = upper)) +
     # Draw 80% threshold
     geom_hline(yintercept = 0.8, color = 'gray70', lty = 2) +
     geom_line(color = 'black') + geom_point(color = 'black') +
-    geom_errorbar(ymin = data.frame(summary(x))$lower, 
-                  ymax = data.frame(summary(x))$upper, 
-                  width = 50, color = 'black') +
-    scale_x_continuous(name = paste('Levels of', x$along), breaks = x$nlevels) +
+    geom_errorbar(width = 50, color = 'black') +
+    scale_x_discrete(name = paste('Levels of', x$along)) +
     scale_y_continuous(name = 'Power', limits = c(0, 1), breaks = c(0, .2, .4, .6, .8, 1),
                        labels = c('0%', '20%', '40%', '60%', '80%', '100%')) +
     theme_classic() + theme(axis.title = element_text(size = 16), 
